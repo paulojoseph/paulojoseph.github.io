c9034e7b2d61158037334e9d180a70ccc3ccdadd
Paulo Marques <79942696+paulojoseph@users.noreply.github.com>
2022-07-18 11:28:26 -0300

    atualização da pasta

--- bwa-django-final-project-main/bwa-django-final-project/inventory/admin.py
+++
@@ -1,6 +0,0 @@
-from django.contrib import admin
-
-# Register your models here.
-from .models import RecipeRequirement, MenuItem, Ingredient, Purchase
-
-[admin.site.register(X) for X in [RecipeRequirement, MenuItem, Ingredient, Purchase]]
